72866009a850053cd36918906bd6594149f6dc24
Otto Åhlfors <75956314+OttoAhlfors@users.noreply.github.com>
2025-04-20 17:59:56 +0300

    My own keypad_handler.c

---
+++ keypad_handler.h
@@ -0,0 +1,21 @@
+﻿/*
+ * keypad_handler.h
+ *
+ * Created: 20.4.2025 12.47.25
+ *  Author: Otto Åhlfors
+ */ 
+
+#ifndef KEYPAD_HANDLER_H
+#define KEYPAD_HANDLER_H
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "keypad.h"
+#include <stdlib.h>
+
+/**
+ * @brief Handles keypad input and updates the LCD with the key pressed.
+ */
+void handle_keypad_input(void);
+
+#endif // KEYPAD_HANDLER_H
