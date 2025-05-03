37a53646a85fe9f06b14a9899736c0259c6fc5e0
OttoAhlfors <otto.ahlfors@gmail.com>
2025-05-03 20:25:18 +0300

    Keypad can handle 2 digits

--- Master/keypad_handler.c
+++ Master/keypad_handler.c
@@ -3,28 +3,35 @@
  *
  * Created: 20.4.2025 12.47.09
  *  Author: Otto Åhlfors
- */ 
+ */
 
 #include "keypad_handler.h"
 #include "lcd_handler.h" // So you can call write_to_lcd()
 
-int handle_keypad_input(void) {
-	uint8_t key_signal = KEYPAD_GetKey();
-	_delay_ms(300);
+int handle_keypad_input(void)
+{
+	char floor[3] = {0}; // to store two digits and null-terminator
+	uint8_t index = 0;
 
-	if (key_signal != 0xFF) { // filter out invalid reads
-		char key_str[4];
-
-		if (key_signal >= '1' && key_signal <= '9') {
-			uint8_t key_value = key_signal - '0';
-			itoa(key_value, key_str, 10);
-		} else {
-			key_str[0] = key_signal;
-			key_str[1] = '\0';
+	while (index < 2)
+	{
+		uint8_t key_signal = KEYPAD_GetKey();
+		if (key_signal != 0xFF && key_signal >= '0' && key_signal <= '9')
+		{
+			digits[index++] = key_signal;
+			_delay_ms(300); // debounce delay
 		}
-	
-		return key_signal;
+		else if (key_signal == '#')
+		{
+			// '#' used to confirm entry
+			break;
+		}
 	}
-	
-	return 0;
+
+	if (index > 0)
+	{
+		return atoi(floor); // convert collected digits to int
+	}
+
+	return 0; // invalid or no input
 }
