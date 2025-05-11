87616d867911470c655150a4b2b02d882ca1b71c
Juhani177 <juhani.juola4@gmail.com>
2025-05-11 20:26:54 +0300

    Update main.c (slave) - Kommentit yritys 2

--- Slave/main.c
+++ Slave/main.c
@@ -24,7 +24,7 @@
 static void USART_init(uint16_t ubrr)
 {
     UBRR0H = (unsigned char)(ubrr >> 8); // set baud rate in UBBR0H and UBBR0L p. 206
-    UBRR0L = (unsigned char)ubrr;
+    UBRR0L = (unsigned char)ubrr; //Setting up the UART
     UCSR0B |= (1 << RXEN0) | (1 << TXEN0); // enable receiver and transmitter in USART register B p. 206, 220
     UCSR0C |= (1 << USBS0) | (3 << UCSZ00); // frame format in UCSRnC
 }
@@ -65,32 +65,33 @@ void emergency(void)
 	PORTD |= (1 << PD7); // Dooropen LED
     TCCR1B |= (1 << 1); // Prescaler set to 8
     
-    uint16_t note = 1000;
+    uint16_t note = 1000; // Declaring a variable for emergency note
     PORTD |= (1 << PD7); // Dooropen
 
     for (int i = 0; i < 6; i++) // Loop for playing emergency note
     {
         OCR1A = note; // Note frequency
-        note = note+750;
-        _delay_ms(500);
+        note = note+750; //Changing the note frequency
+        _delay_ms(500); //Wait for half a second
     }    
          
 	// Disable all previously set settings
-    TCCR1A &= ~(1 << 6);
-    TCCR1A &= ~(1 << 0);
-    TCCR1B &= ~(1 << 4);
-    TIMSK1 &= ~(1 << 1);
+    TCCR1A &= ~(1 << 6); //Clears COM1A
+    TCCR1A &= ~(1 << 0); //Clears WGM10
+    TCCR1B &= ~(1 << 4); //Clears WGM12
+    TIMSK1 &= ~(1 << 1); //Clears OCIE1A
 
-	OCR1A = 0;
-	TCNT1 = 0;
-	_delay_ms(2000);
+	//Resetting timer registers
+	OCR1A = 0; //Set Output Compare register to 0
+	TCNT1 = 0; //Reset Counter1
+	_delay_ms(2000); //Wait for 2 seconds
     PORTD &= ~(1 << PD7); // Close door
 }
 
-ISR (TIMER1_COMPA_vect) {}
+ISR (TIMER1_COMPA_vect) {} //Define an interrupt service routine for Timer1
 
-FILE uart_output = FDEV_SETUP_STREAM(USART_Transmit, NULL, _FDEV_SETUP_WRITE);
-FILE uart_input = FDEV_SETUP_STREAM(NULL, USART_Receive, _FDEV_SETUP_READ);
+FILE uart_output = FDEV_SETUP_STREAM(USART_Transmit, NULL, _FDEV_SETUP_WRITE); //Defining custom output for stdio commands
+FILE uart_input = FDEV_SETUP_STREAM(NULL, USART_Receive, _FDEV_SETUP_READ); //Defining custom input for stdio commands
 
 int main(void)
 {
@@ -99,9 +100,9 @@ int main(void)
     DDRB |= (1 << PB5);  // Emergency LED
     DDRB |= (1 << PB1);  // Buzzer
 
-    USART_init(MYUBBR);
-    stdout = &uart_output;
-    stdin = &uart_input;
+    USART_init(MYUBBR);  // Initializing the USART peripheral with a baud rate.
+    stdout = &uart_output; //Redirecting the output stream to use the UART.
+    stdin = &uart_input; // Redirecting input to read from the UART
 
     // Setup I²C as slave
     TWAR = (SLAVE_ADDRESS << 1);             // Slave address
@@ -116,8 +117,8 @@ int main(void)
 
         twi_status = (TWSR & 0xF8); // Mask prescaler bits
 
-        if ((twi_status == 0x80) || (twi_status == 0x90)) {
-            twi_receive_data = TWDR;
+        if ((twi_status == 0x80) || (twi_status == 0x90)) { //If data has been received and ACK returned
+            twi_receive_data = TWDR; //Store the received data in twi_receive_data
             printf("%d\n", twi_receive_data); // Transmission test print
 
             // React to master's command
@@ -130,14 +131,14 @@ int main(void)
                     break;
                 case 0x03: // Blink movement LED 3x (FAULT)
                     for (int i = 0; i < 3; i++) {
-                        PORTB |= (1 << PB0);
+                        PORTB |= (1 << PB0); //Turn on LED
                         _delay_ms(300);
-                        PORTB &= ~(1 << PB0);
+                        PORTB &= ~(1 << PB0); //Turn off LED
                         _delay_ms(300);
                     }
                     break;
                 case 0x04: // Door LED ON
-                    PORTD |= (1 << PD7);
+                    PORTD |= (1 << PD7); 
                     break;
                 case 0x05: // Door LED OFF
                     PORTD &= ~(1 << PD7);
@@ -149,13 +150,13 @@ int main(void)
         }
         else if ((twi_status == 0x88) || (twi_status == 0x98)) {
             // Data received but NACK'd
-            twi_receive_data = TWDR;
-            printf("NACK");
-            printf("%d\n", twi_receive_data);
+            twi_receive_data = TWDR; //Read the received byte
+            printf("NACK"); //Print "NACK"
+            printf("%d\n", twi_receive_data); //Print the received byte
         }
         else if (twi_status == 0xA0) {
             // STOP condition received
-            TWCR |= (1 << TWINT);
+            TWCR |= (1 << TWINT); //Setting a TWINT flag
         }
 
         TWCR |= (1 << TWINT) | (1 << TWEA) | (1 << TWEN); // Resume listening
